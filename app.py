abdc286612ed66f1265bf7007aae6c381c361be4
VINESH <63443210+vineshbavikkara@users.noreply.github.com>
2020-08-07 15:25:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,16 +12,12 @@ model = pickle.load(open('model.pkl','rb'))
 @app.route('/')
 def home():
     return render_template("index.html")
-
 @app.route('/predict',methods=['POST'])
 def predict():
     response=[x for x in request.form.values()]  
-    
     response = pd.DataFrame(response).T
     response.columns = ['DisbursementGross', 'Term', 'NoEmp', 'NewExist_new','FranchiseCode_yes',
                     'UrbanRural_urban','LowDoc_yes', 'RevLineCr_yes']
-
-    # def of changing to dummy variables value
     def dummy_urban(num):
         if num is 'Urban':
             return 1
@@ -30,8 +26,6 @@ def predict():
             return 0
     
     UrbanRural_urban = response['UrbanRural_urban'].apply(dummy_urban)
-
-    # def of changing to dummy variables value
     def dummy_undefined(num):
         if num is 'Undefined':
             return 1
@@ -40,7 +34,6 @@ def predict():
             return 0
         
     UrbanRural_undefined = response['UrbanRural_urban'].apply(dummy_undefined)
-    
     dummy_data = pd.concat([UrbanRural_urban,UrbanRural_undefined], axis= 1, ignore_index= False)
     dummy_data.columns = ['UrbanRural_urban','UrbanRural_undefined']
     
@@ -55,8 +48,6 @@ def predict():
     if pred[0]==1:
         return render_template("index.html",predicted="High Risk")
     return render_template("index.html",predicted="Low Risk")
-
-
 
 if __name__=="__main__":
     app.run(debug=True)
